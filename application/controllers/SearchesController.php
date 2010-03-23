e1487be3b40000526ebfc08ee81dad66cd6dd6fd
Mariusz <adept_koza@o2.pl>
2010-03-23 23:57:20 +0100

    małe zmiany

--- application/controllers/SearchesController.php
+++ application/controllers/SearchesController.php
@@ -3,36 +3,38 @@
 class SearchesController extends Zend_Controller_Action {
 
 	public function indexAction() {
-
-	}
-
-	public function searchAction() {
 	   	$id = $this->_getParam('id', 0);
   		if ($id > 0) {
-			$this->showAction($id);
+			$monitor  = $this->show_one($id);
+			$monitory = $this->show_all();
 		} else {
-			$monitory = new Application_Model_DbTable_Monitory();
-			$this->view->monitory = $monitory->fetchAll();
+			$monitory = $this->show_all();
 		}
 	}
 
-	public function findAction() {
-		$form = new Application_Form_Znajdz();
+	public function searchAction() {
+		$form = new Application_Form_Search();
 		$this->view->form = $form;
-			if ($this->getRequest()->isPost()) {
-  				$formData = $this->getRequest()->getPost();
-  				if ($form->isValid($formData)) {
-    	 		   	$id = $form->getValue('id');
-   				    $this->_redirect('/searches/search/id/'.$id);	
-//	 	 		   	$nazwa = $form->getValue('nazwa');
-//   			    $monitor = new Application_Model_DbTable_Monitory();
-				}		
+		if ($this->getRequest()->isPost()) {
+			$formData = $this->getRequest()->getPost();
+			if ($form->isValid($formData)) {
+				if ($this->getRequest()->getParam('id')) {
+					$id = $form->getValue('id');					
+					$this->show_one($id);
+				}
 			}
+		}
 	}
 
-	protected function showAction($id) {
-			$monitory = new Application_Model_DbTable_Monitory();
-			$this->view->monitory = $monitory->getMonitor($id);
+
+	private function show_one($id) {
+			$monitor = new Application_Model_DbTable_Monitory();
+			$this->view->monitor = $monitor->getMonitor($id);
+	}
+
+	private function show_all() {
+		$monitory = new Application_Model_DbTable_Monitory();
+		$this->view->monitory = $monitory->fetchAll();
 	}
 
 }
